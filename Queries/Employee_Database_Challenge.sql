318a6cb842754db1d570c1530b5b03b872002354
Noelle Garza <derossogarza@gmail.com>
2021-06-06 22:16:43 -0500

    create mentorship eligibility table

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -27,3 +27,18 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY "emp_count" DESC;
+
+--Employees Eligible for Mentorship Program
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	tl.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS tl ON (e.emp_no = tl.emp_no)
+WHERE de.to_date = ('9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
